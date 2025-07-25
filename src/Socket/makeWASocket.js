dbcdbb0cddd11a1fcf00ed9d3afcbe9bdb03bb0a
✞︎ 𝙇𝙊𝙍𝘿 𝙊𝘽𝙄𝙏𝙊 𝘿𝙀𝙑 ✞ <nguessanjeanstephane87@gmail.com>
2025-07-25 23:38:22 +0000

    Create makeWASocket.js

---
+++ src/Socket/makeWASocket.js
@@ -0,0 +1,7 @@
+import { default as baileys } from '@whiskeysockets/baileys'
+import { useMultiFileAuthState } from '@whiskeysockets/baileys'
+
+const makeWASocket = baileys.default
+
+export default makeWASocket
+export { useMultiFileAuthState }
